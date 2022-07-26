95f5e4aef7b69aacb57c87dc126b9c87dbf341a1
lara123-pg <lamorimalvescavalcante@gmail.com>
2022-07-26 16:04:51 -0300

    Adicionando dados do FAQ em um arquivo JSON para serem mostrados em tela

--- src/page/InitialScreen.jsx
+++ src/page/InitialScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Question } from '../components/Question';
 import { InputEmail } from '../components/InputEmail';
@@ -14,6 +14,33 @@ import info from '../assets/info.png';
 import '../scss/main.scss';
 
 export function InitialScreen() {
+    const [questionsData, setQuestionsData] = useState([]);
+
+    const getData = () => {
+        fetch('../../public/dataQuestions.json', {
+            headers: {
+                Accept: "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(response => setQuestionsData(response));
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const showQuestions = questionsData && questionsData.questions?.map((questionData) => {
+        return (
+            <div key={questionData.id}>
+                <Question 
+                    question = {questionData.question}
+                    response = {questionData.response}
+                />
+            </div>
+        );
+    });
+
     return(
         <div className="container">
             <div className="initialPart">
@@ -123,39 +150,7 @@ export function InitialScreen() {
                 <h1>Perguntas frequentes</h1>
 
                 <div className="faq">
-                    <Question 
-                        question="O que é a Netflix?"
-                        response="A Netflix é um serviço de transmissão online que oferece uma ampla variedade de séries, filmes e documentários premiados em milhares de aparelhos conectados à internet.
-                        
-                        Você pode assistir a quantos filmes e séries quiser, quando e onde quiser, sem comerciais – tudo por um preço mensal bem acessível. Aqui você sempre encontra novidades. A cada semana, adicionamos novas séries e filmes."
-                    />
-
-                    <Question 
-                        question="Quanto custa a Netflix?"
-                        response="Assista à Netflix no seu celular, tablet, Smart TV, notebook ou aparelho de streaming por uma taxa mensal única. Os planos variam de R$25,90 a R$55,90 por mês. Sem contrato nem taxas extras."
-                    />
-
-                    <Question 
-                        question="Onde posso assistir?"
-                        response="Assista onde quiser, quando quiser. Faça login com sua conta Netflix em netflix.com para começar a assistir no computador ou em qualquer aparelho conectado à Internet compatível com o aplicativo Netflix, como Smart TVs, smartphones, tablets, aparelhos de streaming e videogames.
-                        Você também pode baixar a sua série favorita com o aplicativo Netflix para iOS, Android ou Windows 10. Use downloads para levar a Netflix para onde quiser sem precisar de conexão com a Internet. Leve a Netflix com você para qualquer lugar."
-                    />
-
-                    <Question 
-                        question="Como faço para cancelar?"
-                        response="A Netflix é flexível. Não há contratos nem compromissos. Você pode cancelar a sua conta na internet com apenas dois cliques. Não há taxa de cancelamento – você pode começar ou encerrar a sua assinatura a qualquer momento."
-                    />
-
-                    <Question 
-                        question="O que eu posso assistir na Netflix?"
-                        response="A Netflix tem um grande catálogo de filmes, documentários, séries, originais Netflix premiados e muito mais. Assista o quanto quiser, quando quiser."
-                    />
-
-                    <Question 
-                        question="A Netflix é adequada para crianças?"
-                        response="A experiência infantil da Netflix faz parte da sua assinatura para que as crianças se divirtam em seu próprio espaço com séries e filmes familiares sob a supervisão dos responsáveis.
-                        O recurso de controle parental, incluso nos perfis para crianças e protegido por PIN, permite restringir a classificação etária do conteúdo que as crianças podem ver e bloquear títulos específicos que você não quer que elas assistam."
-                    />
+                    {showQuestions}
                 </div>
 
                 <div className="inputInfos">
